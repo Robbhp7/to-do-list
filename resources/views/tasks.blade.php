d9ee61a9d644b3fd26ecce1bcb03d545da9d1d97
Robb <robert.serrano77@gmail.com>
2024-08-24 15:05:59 +0100

    chg: installing bootstrap 5

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -5,19 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MLP To-Do</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
     <link href="{{mix('css/app.css')}}" rel="stylesheet">
 </head>
 <body>
 <div class="container">
-    <div class="col-12 col-md-4">
-        Add
-    </div>
-    <div class="col-12 col-md-8">
-        Table
+    <div class="row">
+        <div class="col-12 col-md-4">
+            Add
+        </div>
+        <div class="col-12 col-md-8">
+            Table
+        </div>
     </div>
 </div>
 <script src="{{asset('js/app.js')}}"></script>
